392f952b41d43afe583852de6d4c56c2048952d8
JosephDonoso <joseph.donoso.c@mail.pucv.cl>
2026-06-12 16:08:34 -0400

    Add command-line argument handling for iteration count and initial seed in bucle execution script

--- scripts/script_ejecucion_bucle.py
+++ scripts/script_ejecucion_bucle.py
@@ -1,7 +1,28 @@
 
-import subprocess, random, sys
-random.seed(9999)
-seeds = [random.randint(1,999999) for _ in range(2)]
+import subprocess, random, sys, os
+
+if len(sys.argv) > 3 or len(sys.argv) < 2:
+    print("Uso: python scripts/script_ejecucion_bucle.py <num_iteraciones> <semilla_inicial>")
+    sys.exit(1)
+
+if len(sys.argv) == 3:
+    try:
+        num_iteraciones = int(sys.argv[1])
+        semilla_inicial = int(sys.argv[2])
+    except ValueError:
+        print("Error: Ambos argumentos deben ser enteros.")
+        sys.exit(1)
+
+if len(sys.argv) == 2:
+    try:
+        num_iteraciones = int(sys.argv[1])
+        semilla_inicial = 999999
+    except ValueError:
+        print("Error: El argumento debe ser un entero.")
+        sys.exit(1)
+
+random.seed(semilla_inicial)
+seeds = [random.randint(1,999999) for _ in range(num_iteraciones)]
 for s in seeds:
     print(f'\n=== Seed {s} ===')
     subprocess.run([sys.executable, 'scripts/generador_instancias.py', '--config', 'config/config.generador.json', '--seed', str(s)])
